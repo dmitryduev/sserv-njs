a0f68a98dd4d4905f32dc9e11c33c90e98c6d984
Dmitry Duev <dmitry.duev@gmail.com>
2016-01-13 12:27:48 -0800

    added png daemon

---
+++ png-daemon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#until watch -n 0.5 ./lib/png2 /Users/dmitryduev/web/sserv-njs/public /Users/dmitryduev/web/sserv/telemetry; do
+#    echo "'png2' crashed with exit code $?. Restarting..." >&2
+#    sleep 1
+#done
+#until while true; do ./lib/png2 /Users/dmitryduev/web/sserv-njs/public /Users/dmitryduev/web/sserv/telemetry; sleep 0.5; done
+until while true; do ./lib/png2 /home/roboao/web/sserv-njs/public /home/roboao/Status; sleep 0.5; done
+do
+   echo "'png2' crashed with exit code $?. Rerunning..." >&2
+   sleep 1
+done
